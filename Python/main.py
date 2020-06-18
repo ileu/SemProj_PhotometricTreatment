e35f0df03219aa6a6ebc51deb213039ed8f32331
ileu <usauterv@outlook.ocm>
2020-06-18 18:22:10 +0200

    Changed fitting method Added better save function

--- Python/main.py
+++ Python/main.py
@@ -2,10 +2,13 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.optimize import curve_fit, root_scalar
 from StarData import cyc116, ND4, PointSpread
+from datetime import datetime
+from scipy.ndimage import gaussian_filter1d
 from StarFunctions import azimuthal_averaged_profile, magnitude_wavelength_plot, diffraction_rings, \
     half_azimuthal_averaged_profile, annulus
 import StarGUI
 import DiskGUI
+
 
 # StarGUI.start(HD163296)
 
@@ -15,20 +18,27 @@ import DiskGUI
 
 # plt.show()
 
-first = 12
-second = 45
-y_min = 0.1
-tail = np.linspace(120, 200, 8, dtype=int, endpoint=False)
-nd4_region = np.concatenate((np.arange(first, second), tail))
-psf_region = np.concatenate((np.arange(20), tail))
-markers_on = [first, second, *tail]
-markers_on2 = [20, *tail]
-
-profile = ["I-band", "R-band"]
-
-
 def scaling_func(pos, a, b):
     return a * (pos - b)
+
+
+def scaling_gauss_func(pos, a, b, sig):
+    return a * (gaussian_filter1d(pos, sig) - b)
+
+
+def mkdir_p(mypath):
+    '''Creates a directory. equivalent to using mkdir -p on the command line'''
+
+    from errno import EEXIST
+    from os import makedirs, path
+
+    try:
+        makedirs(mypath)
+    except OSError as exc:  # Python >2.5
+        if exc.errno == EEXIST and path.isdir(mypath):
+            pass
+        else:
+            raise
 
 
 def align_yaxis(ax1, v1, ax2, v2):
@@ -54,6 +64,33 @@ def adjust_yaxis(ax, ydif, v):
     ax.set_ylim(nminy + v, nmaxy + v)
 
 
+first = 14
+second = 65
+peak = 32
+y_min = 0.1
+tail = np.linspace(120, 200, 8, dtype=int, endpoint=False)
+
+nd4_region = np.concatenate((np.arange(first, second), tail))
+psf_region = np.concatenate((np.arange(first, peak), tail))
+
+markers_on_nd4 = [first, second, *tail]
+markers_on_psf = [peak, *tail]
+weights_nd4 = np.concatenate((np.full((21 - first,), 2), np.full((second - 21,), 1), np.full_like(tail, 1000)))
+weights_psf = np.concatenate((np.full((peak - first,), 3.5), np.full_like(tail, 1)))
+bounds_psf = ([0, -np.inf, 0], np.inf)
+save = True
+smart = False
+mixed = True
+
+profile = ["I-band", "R-band"]
+
+if save:
+    folder = datetime.now().strftime('%d_%m_%H%M')
+    path = "../Bilder/" + folder
+    mkdir_p(path)
+
+    param_file = open(path + "/parameters.txt", "w")
+
 for index in [0, 1]:
     print(profile[index])
     x, radial1 = cyc116.azimuthal[index]
@@ -67,63 +104,64 @@ for index in [0, 1]:
     print("guess: ", guess)
 
     scaling_factor = curve_fit(scaling_func, radial2[nd4_region], radial1[nd4_region], p0=guess,
-                               sigma=radial1[nd4_region])
+                               sigma=weights_nd4)
+
     scaled_profile = scaling_func(radial2, *scaling_factor[0])
 
-    psf_factor = curve_fit(scaling_func, psf[psf_region], scaled_profile[psf_region], p0=guess,
-                           sigma=scaled_profile[psf_region])
+    mixed_profile = radial1.copy()
+    mixed_profile[:80] = scaled_profile[:80]
+    if mixed:
+        scaled_profile = mixed_profile
+        name = "mixed ND4 profile"
+    else:
+        name = "scaled profile of ND4"
 
-    psf_factor1 = scaling_func(radial2[0], *scaling_factor[0]) / psf[0]
+    if smart:
+        tail = np.array((np.abs(scaled_profile[120:200] - radial1[120:200]) / radial1[120:200] <= 0.6827 / 2).nonzero()) \
+               + 120
+        print("Points satisfy condition: ", len(tail[0]))
+        tail = tail[0, ::-len(tail[0]) // 8]
+        tail = tail[::-1]
+        psf_region = np.concatenate((np.arange(peak), tail))
+        weights_psf = np.concatenate((np.full((peak,), 4.50), np.full_like(tail, 1)))
+        markers_on_psf = [peak, *tail]
 
-    psf_profile = scaling_func(psf, *psf_factor[0])
+    psf_factor = curve_fit(scaling_gauss_func, psf[psf_region], scaled_profile[psf_region],
+                           sigma=weights_psf, bounds=bounds_psf)
+
+    psf_profile = scaling_gauss_func(psf, *psf_factor[0])
 
     print("scaling factor", scaling_factor)
     print("psf factor", psf_factor)
-    print("psf factor1", psf_factor1)
 
-    fig = plt.figure(figsize=(28, 14))
-    textax = plt.axes([0.5, 0.95, 0.3, 0.03])
+    fig_comp = plt.figure(figsize=(14, 7))
+    textax = plt.axes([0.5, 0.95, 0.3, 0.03], figure=fig_comp)
     textax.axis('off')
     textax.text(0, 0, "Comparison " + profile[index] + " of ND4 to cyc116", fontsize=18, ha='center')
-
-    # ax = fig.add_subplot(1, 2, 1)
-    # ax.set_title("manual")
-    # ax.semilogy(x, radial1, '-D', label="profile of cyc116", markevery=markers_on)
-    # ax.semilogy(x, guess[0] * (radial2 - guess[1]), label="scaled profile of ND4")
-    #
-    # ax.semilogy(x, psf_factor1 * psf, label="PSF profile")
-    # ax.legend()
-    # ax.set_ylim(ymin=y_min)
-
-    ax = fig.add_subplot(1, 1, 1)
-    ax.semilogy(x, radial1, '-D', label="profile of cyc116", markevery=markers_on)
-    ax.semilogy(x, scaled_profile, label="scaled profile of ND4")
-
-    ax.semilogy(x, psf_profile, '-D', label="PSF profile", markevery=markers_on2)
+    ax = fig_comp.add_subplot(1, 1, 1)
+    ax.semilogy(x, radial1, '-D', label="profile of cyc116", markevery=markers_on_nd4)
+    ax.semilogy(x, scaled_profile, '-D', label=name, markevery=list(tail))
+    ax.semilogy(x, psf_profile, '-D', label="PSF profile", markevery=markers_on_psf)
     ax.legend(fontsize='large')
     ax.set_ylim(ymin=y_min)
 
+    zoom_xax = (5, 63)
+
     axins = ax.inset_axes([0.35, 0.55, 0.5, 0.43])
-    axins.semilogy(x, radial1, '-D', label="profile of cyc116", markevery=markers_on)
-    axins.semilogy(x, scaled_profile, label="scaled profile of ND4")
-    axins.semilogy(x, psf_profile, '-D', label="PSF profile", markevery=markers_on2)
-    axins.set_ylim((0.9 * np.min(psf_profile[:110]), 1.2 * 1e6))
-    axins.set_xlim((-2, 110))
+    axins.semilogy(x, radial1, '-D', label="profile of cyc116", markevery=markers_on_nd4)
+    axins.semilogy(x, scaled_profile, '-D', label=name, markevery=list(tail))
+    axins.semilogy(x, psf_profile, '-D', label="PSF profile", markevery=markers_on_psf)
+    axins.set_ylim(
+        (0.9 * np.min(psf_profile[zoom_xax[0]:zoom_xax[1]]), 1.05 * np.max(psf_profile[zoom_xax[0]:zoom_xax[1]])))
+    axins.set_xlim(zoom_xax)
     ax.indicate_inset_zoom(axins)
 
-    fig.savefig("../Bilder/Comparison_" + profile[index] + ".png", dpi=150)
-
-    fig = plt.figure(figsize=(32, 14))
-    textax = plt.axes([0.5, 0.95, 0.3, 0.03])
+    fig_sub = plt.figure(figsize=(16, 7))
+    textax = plt.axes([0.5, 0.95, 0.3, 0.03], figure=fig_sub)
     textax.axis('off')
     textax.text(0, 0, "Subtraction in " + profile[index], fontsize=18, ha='center')
 
-    # ax = fig.add_subplot(1, 3, 1)
-    # ax.set_title("manual")
-    # ax.plot(x, radial1 - psf_factor1 * psf, )
-    # ax.set_ylim(ymin=-100, ymax=1.1 * max(radial1[20:] - psf_factor1 * psf[20:]))
-
-    ax = fig.add_subplot(1, 1, 1)
+    ax = fig_sub.add_subplot(1, 1, 1)
     line1, = ax.plot(x, radial1 - psf_profile, label="Reduced cyc116 profile")
     ax1 = ax.twinx()
     line2, = ax1.plot(x2[20:], qphi[20:], "C3", label="Qphi profile")
@@ -134,20 +172,32 @@ for index in [0, 1]:
     line3 = ax.axhline(0, ls='--', c='k', alpha=0.5, label="zero")
     lines = [line1, line2, line3]
     align_yaxis(ax, 0, ax1, 0)
-    ax.legend(lines, [line.get_label() for line in lines],fontsize='large')
+    ax.legend(lines, [line.get_label() for line in lines], fontsize='large')
 
-    # ax = fig.add_subplot(1, 3, 3)
-    # ax.set_title("Qphi profile")
-    # ax.plot(x2, qphi)
-    # ax.set_ylim(ymin=-100, ymax=1.1 * max(qphi[20:]))
-    # plt.show()
-    #
-    fig.savefig("../Bilder/Subtraction_" + profile[index] + ".png", dpi=150)
+    if save:
+        fig_comp.savefig(path + "/Comparison_" + profile[index] + ".png", dpi=150)
+        fig_sub.savefig(path + "/Subtraction_" + profile[index] + ".png", dpi=150)
+
+        param_file.write("\n" + profile[index] + "\n")
+        param_file.write("Mixed: {}\n".format(mixed))
+        param_file.write("Smart: {}\n".format(smart))
+        param_file.write("ND4:\n")
+        param_file.write("Region: {}\n".format(nd4_region))
+        param_file.write("Weights: {}\n".format(weights_nd4))
+
+        param_file.write("PSF:\n")
+        param_file.write("Region: {}\n".format(psf_region))
+        param_file.write("Weights: {}\n".format(weights_psf))
+        param_file.write("\nScaling factor: {}\n".format(scaling_factor[0]))
+        param_file.write(("PSF factor: {}\n".format(psf_factor[0])))
 
     disk_profile = radial1 - psf_profile
     disk_profile[disk_profile < 0] = 0
 
     print("Counts fit: ", np.sum(disk_profile[32:125]))
+
+if save:
+    param_file.close()
 
 """ 2d attempt """
 
